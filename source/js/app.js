f07199dc36fbb2b788b52140d2a5ce852ed89c00
Adebiyi Adedotun <AdebiyiAdedotun9@gmail.com>
2018-06-07 01:11:57 +0100

    refacctored the code

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,18 @@
-function showNav() {
-    console.log("this is working");
-    var x = document.getElementById("responsive-nav");
-    if (x.className === "responsive-nav") {
-        x.className += " unfold"; 
-    } else {
-        x.className = "responsive-nav";
-    }
+// I wished I could use ES6 for this. However, the setup is non-trivial
+
+var navButton = document.getElementById("nav-toggle-button");
+
+function toggleNavDisplay(event) {
+    // prevent hyperlink from running
+    event.preventDefault();
+
+    // get the resonsive-nav container
+    var responsiveNav = document.getElementById("responsive-nav");
+
+    // toggle the showNav CSS on the responsive nav container
+    // if the showNav class is there, it is removed; else it is added
+    // with a single property: toggle
+    responsiveNav.classList.toggle("showNav");
 }
+
+navButton.addEventListener("click", toggleNavDisplay);
