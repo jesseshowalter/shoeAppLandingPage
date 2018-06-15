3a0bcbee69261e4da22c57a6ded8695965add513
Jesse Showalter <iamjesseshowalter@gmail.com>
2018-06-14 22:43:29 -0500

    added scroll reveal

--- source/js/app.js
+++ source/js/app.js
@@ -7,3 +7,42 @@ function showNav() {
         x.className = "responsive-nav";
     }
 }
+
+window.sr = ScrollReveal();
+
+sr.reveal('#header img.phone',{
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.car',{
+    delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.shoe',{
+    delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.device',{
+    delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#discount .offer',{
+    delay: 600,
+    duration: 300,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('#discount picture',{
+    delay: 600,
+    duration: 300,
+    origin: 'left',
+    distance: '200px'
+});
